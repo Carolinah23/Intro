53b85376694fb5f3dbbc225906a97243418891ae
Carolinah23 <carolinah0308@hotmail.com>
2022-04-22 21:29:34 -0500

    Final Homework 5

--- Assigment5.py
+++ Assigment5.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-import vtk, inspect
+import vtk
 
 ### Hw 5 - April/22/2022
 ### Alan Braeley - Carolina Hurtado-Pulido
@@ -14,38 +14,25 @@ class MyInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
         if(parent is not None):
           self.parent = parent
      
-
-
-#Loader for our structured dataset
+#Loads the data
 imageReader = vtk.vtkStructuredPointsReader()
 imageReader.SetFileName("./data/foot.vtk")
 imageReader.Update()
 
-#create an outline that shows the bounds of our dataset
-outline = vtk.vtkOutlineFilter();
-outline.SetInputConnection(imageReader.GetOutputPort());
-
-#mapper to push the outline geometry to the graphics library
-outlineMapper = vtk.vtkPolyDataMapper();
-outlineMapper.SetInputConnection(outline.GetOutputPort());
-#actor for the outline to add to our renderer
-outlineActor = vtk.vtkActor();
-outlineActor.SetMapper(outlineMapper);
-outlineActor.GetProperty().SetLineWidth(2.0);
-
-# Transfer mapping
+'''
+Question 1
+'''
+# Transfer mapping to control how opaque the tissue is
 opacityTransferFunction = vtk.vtkPiecewiseFunction()
 opacityTransferFunction.AddPoint(20, 0.0)
-opacityTransferFunction.AddPoint(100, 0.4)
-opacityTransferFunction.AddPoint(200, 0.8)
+opacityTransferFunction.AddPoint(255, 0.5)
 
-# Transfer mapping scalar values to colors
+# Transfer mapping to control the colors
 colorTransferFunction = vtk.vtkColorTransferFunction()
-colorTransferFunction.AddRGBPoint(0, 2.0, 0.0, 0.0)
-colorTransferFunction.AddRGBPoint(64, 5.0, 0.0, 0.0)
-colorTransferFunction.AddRGBPoint(0, 0.0, 128.0, 1.0)
-colorTransferFunction.AddRGBPoint(192, 0.0, 1.0, 0.0)
-colorTransferFunction.AddRGBPoint(255, 0.0, 0.2, 1.0)
+colorTransferFunction.AddRGBPoint(32.0, 0.0, 0.0, 0.0)
+colorTransferFunction.AddRGBPoint(64.0, 1.0, 0.0, 0.0)
+colorTransferFunction.AddRGBPoint(100.0, 0.0, 0.6, 0.5)
+colorTransferFunction.AddRGBPoint(255.0, 1.0, 1.0, 1.0)
 
 
 volumeProperty = vtk.vtkVolumeProperty()
@@ -59,98 +46,80 @@ volumeMapper = vtk.vtkGPUVolumeRayCastMapper()
 volumeMapper.SetBlendModeToComposite()
 volumeMapper.SetInputConnection(imageReader.GetOutputPort())
 
-# The volume holds the mapper and the property and
-# can be used to position/orient the volume
+# The volume holds the mapper and the property and also controls the position
 volume = vtk.vtkVolume()
 volume.SetMapper(volumeMapper)
 volume.SetProperty(volumeProperty)
-
-
-########################################################################################################################################################################
-
 '''
-Need to create a clipping plane with vtkImplicitPlaneWidget2
+End of question 1
 '''
 
-'''#A renderer that renders our geometry into the render window
-renderer = vtk.vtkRenderer()
-renderer.SetBackground(0.1, 0.1, 0.3)
 
 
-#The render window
-renwin = vtk.vtkRenderWindow()
-renwin.AddRenderer(renderer)
-renderer.AddVolume(volume)
-renderer.AddActor(outlineActor)
-renwin.SetSize(600, 600)
-renwin.Render()'''
-
-
-rt = vtk.vtkRTAnalyticSource()
-rt.SetWholeExtent(-10,10,-10,10,-10,10)
-rt.Update()
-
-plane = vtk.vtkPlane()
-clipper = vtk.vtkCutter()
-clipper.SetInputConnection(rt.GetOutputPort())
-clipper.SetCutFunction(plane)
-
-selectMapper = vtk.vtkPolyDataMapper()
-selectMapper.SetInputConnection(clipper.GetOutputPort())
-selectMapper.SetScalarModeToUsePointFieldData()
-selectMapper.SetColorModeToMapScalars()
-selectMapper.ScalarVisibilityOn()
-selectMapper.SetScalarRange(rt.GetOutputDataObject(0).GetPointData().GetScalars().GetRange())
-selectMapper.SelectColorArray('RTData')
-
-selectActor = vtk.vtkActor()
-selectActor.SetMapper(selectMapper)
-selectActor.VisibilityOn()
-selectActor.SetScale(10, 10, 10)
-
-
+'''
+Question 2
+'''
 #A renderer that renders our geometry into the render window
 renderer = vtk.vtkRenderer()
 renderer.SetBackground(0.1, 0.1, 0.3)
 
+#create an outline that shows the bounds of our dataset
+outline = vtk.vtkOutlineFilter();
+outline.SetInputConnection(imageReader.GetOutputPort());
+
+#mapper to push the outline geometry to the graphics library
+outlineMapper = vtk.vtkPolyDataMapper();
+outlineMapper.SetInputConnection(outline.GetOutputPort());
+#actor for the outline to add to our renderer
+outlineActor = vtk.vtkActor();
+outlineActor.SetMapper(outlineMapper);
+outlineActor.GetProperty().SetLineWidth(2.0);
+
 #The render window
 renwin = vtk.vtkRenderWindow()
 renwin.AddRenderer(renderer)
 renderer.AddVolume(volume)
-renderer.AddActor(outlineActor)
-renwin.SetSize(600, 600)
+
+renwin.SetSize(1000, 1000)
 renwin.Render()
-# The callback function
-def myCallback(obj, event):
-    global plane
-    obj.GetRepresentation().GetPlane(plane)
-    selectActor.VisibilityOn()
-
-# Create the representation and widget
-planeRep = vtk.vtkImplicitPlaneRepresentation()
-planeRep.SetPlaceFactor(1.25)
-planeRep.PlaceWidget(rt.GetOutput().GetBounds())
-planeRep.VisibilityOn()
-planeRep.SetNormal(1,1,1)
-planeRep.GetPlane(plane)
-
-planeWidget = vtk.vtkImplicitPlaneWidget2()
-#planeWidget.SetInteractor(iren)
-planeWidget.SetRepresentation(planeRep)
-planeWidget.SetEnabled(1)
-#planeWidget.AddObserver("InteractionEvent", myCallback)
-
-renderer.AddActor(selectActor)
-
-###################################################################################################################################################################################################
 
 #Interactor to handle mouse and keyboard events
 interactor = vtk.vtkRenderWindowInteractor()
 interactor.SetInteractorStyle(MyInteractorStyle(parent = interactor))
 interactor.SetRenderWindow(renwin)
-#iren = vtk.vtkRenderWindowInteractor()
-#iren.SetRenderWindow(renwin)
 
+# Cut the volume
+rta = vtk.vtkRTAnalyticSource()
+rta.SetWholeExtent(0, 255, 0, 255, 0, 255)
+rta.Update()
+
+plane = vtk.vtkPlane()
+plane.SetOrigin(10,10,10)
+plane.SetNormal(2,1,1.5)
+volumeMapper.AddClippingPlane(plane)
+
+# Widget to manipulate the plane
+# The cut plane
+def MovePlane(widget, event_string):
+    rep.GetPlane(plane)
+
+rep = vtk.vtkImplicitPlaneRepresentation()
+rep.SetPlaceFactor(1.0);
+rep.PlaceWidget(rta.GetOutput().GetBounds())
+rep.DrawPlaneOff()
+rep.SetPlane(plane)
+
+planeWidget = vtk.vtkImplicitPlaneWidget2()
+planeWidget.SetInteractor(interactor)
+planeWidget.SetRepresentation(rep);
+planeWidget.AddObserver("InteractionEvent",MovePlane);
+planeWidget.On()
+
+renderer.AddActor(outlineActor)
+renderer.ResetCamera()
+'''
+End of question 2
+'''
 
 interactor.Initialize()
 interactor.Start()
